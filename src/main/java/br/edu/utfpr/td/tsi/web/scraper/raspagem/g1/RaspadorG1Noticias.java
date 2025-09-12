e54c32136c9c8904a5bff550767d98113678aa87
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2025-09-11 22:03:03 -0300

    Exemplo inicial da implementação de raspagem
    - this.logger para fazer os logs, a classe abstrata já inicializa automaticamente de qual classe o logger deve efetuar
    - Utiliza irPara navegação
    - fecharWebDriver para finalizar o navegador

--- src/main/java/br/edu/utfpr/td/tsi/web/scraper/raspagem/g1/RaspadorG1Noticias.java
+++ src/main/java/br/edu/utfpr/td/tsi/web/scraper/raspagem/g1/RaspadorG1Noticias.java
@@ -9,25 +9,39 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import br.edu.utfpr.td.tsi.web.scraper.raspagem.AbstractRaspador;
-import br.edu.utfpr.td.tsi.web.scraper.raspagem.modelos.Noticias;
+import br.edu.utfpr.td.tsi.web.scraper.raspagem.modelos.Noticia;
 
 @Component
-public class RaspadorG1Noticias extends AbstractRaspador<Noticias> {
-	@Override
-	public List<Noticias> raspar() {
-		Document document = irPara("https://g1.globo.com", Duration.ofSeconds(3));
-		
-		List<Noticias> noticias = new ArrayList<Noticias>();
-		
-	    Elements listaDeNoticias =	document.select("feed-post-body");
+public class RaspadorG1Noticias extends AbstractRaspador<Noticia> {
+   
 
-	    for(var i = 0; i < listaDeNoticias.size(); i++) {
-	      String manchete = listaDeNoticias.get(i).getElementsByAttribute("elementtiming").text();
-	      System.out.print(manchete);
-	    }
-	    
-	    fecharWebDriver();
-	    
+	@Override
+	public List<Noticia> raspar() {
+		this.logger.info("Iniciando raspagem noticias G1");
+		
+		Document document = irPara("https://g1.globo.com", Duration.ofSeconds(3));
+
+		List<Noticia> noticias = new ArrayList<Noticia>();
+
+		Elements listaDeNoticias = document.select(".feed-post-body");
+
+		for (var i = 0; i < listaDeNoticias.size(); i++) {
+			Noticia noticia = new Noticia();
+			Elements tituloNoticia = listaDeNoticias.get(i).select(".feed-post-body-title");
+			String manchete = tituloNoticia.text();
+			String linkNoticia = tituloNoticia.attr("href");
+
+			noticia.setLinkNoticia(linkNoticia);
+			noticia.setManchete(manchete);
+
+			System.out.print(manchete + linkNoticia + "\n");
+			noticias.add(noticia);
+		}
+
+		fecharWebDriver();
+
+		this.logger.info("Finalizado raspagem noticias G1");
+		
 		return noticias;
 	}
 }
