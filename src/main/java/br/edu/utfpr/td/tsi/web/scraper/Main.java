1b98789ed0abb007fb3b4b750323fca564c0ad56
Altran <altranfellipe@gmail.com>
2025-09-21 18:55:46 -0300

    Ajustado confiImoveis na main

--- src/main/java/br/edu/utfpr/td/tsi/web/scraper/Main.java
+++ src/main/java/br/edu/utfpr/td/tsi/web/scraper/Main.java
@@ -1,10 +1,20 @@
 package br.edu.utfpr.td.tsi.web.scraper;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.edu.utfpr.td.tsi.web.scraper.etl.CarregadorArquivosJson;
+import br.edu.utfpr.td.tsi.web.scraper.etl.ExtratorListaItemsArquivosJson;
+import br.edu.utfpr.td.tsi.web.scraper.etl.Job;
+import br.edu.utfpr.td.tsi.web.scraper.etl.Transformador;
 import br.edu.utfpr.td.tsi.web.scraper.persistencia.GravadorArquivoJson;
+import br.edu.utfpr.td.tsi.web.scraper.raspagem.imoveis.ImovelTransformador;
+import br.edu.utfpr.td.tsi.web.scraper.raspagem.imoveis.RaspadorImoveis;
+import br.edu.utfpr.td.tsi.web.scraper.raspagem.modelos.Imovel;
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
@@ -13,6 +23,12 @@ public class Main {
 	@Autowired
 	private GravadorArquivoJson gravadorArquivoJson;
 
+	@Value("${file.output}")
+	private String output;
+
+	@Value("${file.input}")
+	private String input;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 
@@ -20,6 +36,25 @@ public class Main {
 
 	@PostConstruct
 	public void raspagemJsoup() {
-		
+		configImoveis();
+	}
+
+	public void configImoveis() {
+		List<Imovel> imoveis = new RaspadorImoveis().raspar();
+		gravadorArquivoJson.gravarArquivo(imoveis, "imoveis");
+
+		ExtratorListaItemsArquivosJson<Imovel> extrator = new ExtratorListaItemsArquivosJson<Imovel>();
+		extrator.setListType(Imovel.class);
+		extrator.setInput(input);
+
+		Transformador<Imovel, Imovel> transformador = new ImovelTransformador();
+		CarregadorArquivosJson<Imovel> carregador = new CarregadorArquivosJson<Imovel>();
+		carregador.setOutput(output);
+
+		Job<Imovel, Imovel> job = new Job<Imovel, Imovel>();
+		job.setExtrator(extrator);
+		job.setTransformador(transformador);
+		job.setCarregador(carregador);
+		job.executar();
 	}
 }
