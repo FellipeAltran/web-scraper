339fab3679290d1a12b5a63bcd4d221aa9a89d98
RafaelGasparoto <rafaelgasparoto4@gmail.com>
2025-11-22 18:05:51 -0300

    Create NoticiaRelacionada.java

---
+++ src/main/java/br/edu/utfpr/td/tsi/web/scraper/raspagem/modelos/NoticiaRelacionada.java
@@ -0,0 +1,25 @@
+package br.edu.utfpr.td.tsi.web.scraper.raspagem.modelos;
+
+public class NoticiaRelacionada {
+	String manchete;
+	String linkNoticia;
+
+	public NoticiaRelacionada() {
+	}
+
+	public void setLinkNoticia(String linkNoticia) {
+		this.linkNoticia = linkNoticia;
+	}
+
+	public void setManchete(String manchete) {
+		this.manchete = manchete;
+	}
+
+	public String getLinkNoticia() {
+		return linkNoticia;
+	}
+
+	public String getManchete() {
+		return manchete;
+	}
+}
